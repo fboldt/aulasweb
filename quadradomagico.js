e9783d396f2734997195d26f980c060a02a07c86
Francisco Boldt <fboldt@gmail.com>
2021-08-12 17:48:45 -0300

    verifica números repetidos e fora dos limites

--- quadradomagico.js
+++ quadradomagico.js
@@ -1,5 +1,8 @@
 const ordem = 3;
-const numerosDisponiveis = [...Array(ordem**2+1).keys()].slice(1);
+const matriz = Array(ordem);
+for (let i=0; i<matriz.length; i++) {
+    matriz[i] = Array(ordem);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     insereTabela(ordem);
@@ -14,36 +17,68 @@ function insereTabela(ordem=3) {
         for (let j=0; j<ordem; j++) {
             const celula = document.createElement('td');
             linha.append(celula);
+            celula.id = `lin${i}col${j}`;
             insereInput(celula);
-            celula.id = `${i}:${j}`;
         }
     }
 }
 
+function getLinhaColuna(celula) {
+    const [linha,coluna] = celula.id.split('col');
+    return [linha.split('lin')[1], coluna];
+}
+
 function insereInput(celula) {
     const input = document.createElement('input');
-    input.maxLength = 1;
     celula.append(input);
     input.addEventListener('change', () => {
         const valor = parseInt(input.value);
-        const valorAntigo = parseInt(celula.dataset.valor);
-        console.log(valor,valorAntigo);
-        if (!isNaN(valorAntigo) && valorAntigo != valor) {
-            delete celula.dataset.valor;
-            numerosDisponiveis[valorAntigo-1] = valorAntigo;
-        }
-        if (valorPermitido(valor)) {
-            celula.dataset.valor = valor;
-            delete numerosDisponiveis[valor-1];
-            input.classList.remove("errado");
-        } else {
-            input.classList.add("errado");
-        }
-        console.log(numerosDisponiveis);
+        const [linha,coluna] = getLinhaColuna(celula);
+        matriz[linha][coluna] = valor;
+        verificaMatriz();
     });
 }
 
-function valorPermitido(valor) {
-    const valorDisponivel = numerosDisponiveis.includes(valor);
-    return valorDisponivel;
+function verificaMatriz() {
+    console.log(matriz);
+    verificaNumerosForaDosLimites();
+    verificaNumerosRepetidos();
+}
+
+function verificaNumerosForaDosLimites() {
+    const minimo = 1;
+    const maximo = ordem**2;
+    for (let i=0; i<ordem; i++) {
+        for (let j=0; j<ordem; j++) {
+            const celula = document.querySelector(`#lin${i}col${j}`);
+            if (matriz[i][j] < minimo || matriz[i][j] > maximo) {
+                celula.classList.add("foradoslimites");
+            } else {
+                celula.classList.remove("foradoslimites");
+            }
+        }
+    }
+}
+
+function verificaNumerosRepetidos() {
+    numeros = Array(ordem**2).fill(0);
+    for (let i=0; i<ordem; i++) {
+        for (let j=0; j<ordem; j++) {
+            const valor = matriz[i][j];
+            if (!isNaN(valor)) {
+                numeros[valor-1]++;
+            }
+        }
+    }
+    for (let i=0; i<ordem; i++) {
+        for (let j=0; j<ordem; j++) {
+            const valor = matriz[i][j];
+            const celula = document.querySelector(`#lin${i}col${j}`);
+            if (!isNaN(valor) && numeros[valor-1] > 1) {
+                celula.classList.add("numerosrepetidos");
+            } else {
+                celula.classList.remove("numerosrepetidos");
+            }
+        }
+    }
 }
